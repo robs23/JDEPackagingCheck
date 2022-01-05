e62e737cf4669d054fc6c52d74b1b35c43995609
Robert Roszak <robert.roszak@outlook.com>
2022-01-05 11:52:02 +0100

    Add project files.

---
+++ JDEPackagingCheck/JDEPackagingCheckRibbon.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace JDEPackagingCheck
+{
+    partial class JDEPackagingCheckRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public JDEPackagingCheckRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabJDEPackaging = this.Factory.CreateRibbonTab();
+            this.grpScheduleKontrol = this.Factory.CreateRibbonGroup();
+            this.btnShowCoverage = this.Factory.CreateRibbonButton();
+            this.btnHideCoverage = this.Factory.CreateRibbonButton();
+            this.tabJDEPackaging.SuspendLayout();
+            this.grpScheduleKontrol.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabJDEPackaging
+            // 
+            this.tabJDEPackaging.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tabJDEPackaging.Groups.Add(this.grpScheduleKontrol);
+            this.tabJDEPackaging.Label = "JDE Opakowania";
+            this.tabJDEPackaging.Name = "tabJDEPackaging";
+            // 
+            // grpScheduleKontrol
+            // 
+            this.grpScheduleKontrol.Items.Add(this.btnShowCoverage);
+            this.grpScheduleKontrol.Items.Add(this.btnHideCoverage);
+            this.grpScheduleKontrol.Label = "Kontrola harmonogramu";
+            this.grpScheduleKontrol.Name = "grpScheduleKontrol";
+            // 
+            // btnShowCoverage
+            // 
+            this.btnShowCoverage.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnShowCoverage.Image = global::JDEPackagingCheck.Properties.Resources.colorizeRows;
+            this.btnShowCoverage.Label = "Pokaż pokrycie";
+            this.btnShowCoverage.Name = "btnShowCoverage";
+            this.btnShowCoverage.ShowImage = true;
+            this.btnShowCoverage.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnShowCoverage_Click);
+            // 
+            // btnHideCoverage
+            // 
+            this.btnHideCoverage.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnHideCoverage.Image = global::JDEPackagingCheck.Properties.Resources.hideCoverage;
+            this.btnHideCoverage.Label = "Ukryj pokrycie";
+            this.btnHideCoverage.Name = "btnHideCoverage";
+            this.btnHideCoverage.ShowImage = true;
+            this.btnHideCoverage.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnHideCoverage_Click);
+            // 
+            // JDEPackagingCheckRibbon
+            // 
+            this.Name = "JDEPackagingCheckRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tabJDEPackaging);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.JDEPackagingCheckRibbon_Load);
+            this.tabJDEPackaging.ResumeLayout(false);
+            this.tabJDEPackaging.PerformLayout();
+            this.grpScheduleKontrol.ResumeLayout(false);
+            this.grpScheduleKontrol.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tabJDEPackaging;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpScheduleKontrol;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnShowCoverage;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnHideCoverage;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal JDEPackagingCheckRibbon JDEPackagingCheckRibbon
+        {
+            get { return this.GetRibbon<JDEPackagingCheckRibbon>(); }
+        }
+    }
+}
---
+++ JDEPackagingCheck/JDEPackagingCheckRibbon.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JDEPackagingCheck
+{
+    public partial class JDEPackagingCheckRibbon
+    {
+        public int StockColumn { get; set; } = 7;
+        public int FirstColumn { get; set; } = 10;
+        private void JDEPackagingCheckRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnShowCoverage_Click(object sender, RibbonControlEventArgs e)
+        {
+            Workbook wb = Globals.ThisAddIn.Application.ActiveWorkbook;
+            Worksheet sht = wb.ActiveSheet;
+
+            Range UsedRange = sht.UsedRange;
+            foreach(Range row in UsedRange.Rows)
+            {
+                double stock = 0;
+
+                if(((Range)UsedRange[row.Row, StockColumn]).Value != null)
+                {
+                    bool isNumber = double.TryParse(((Range)UsedRange[row.Row, StockColumn]).Value.ToString(), out stock);
+                    if (isNumber)
+                    {
+                        for (int i = FirstColumn; i < UsedRange.Columns.Count-1; i++)
+                        {
+                            double currReq = 0;
+                            if(((Range)UsedRange[row.Row, i]).Value != null)
+                            {
+                                string val = ((Range)UsedRange[row.Row, i]).Value.ToString();
+                                isNumber = double.TryParse(((Range)UsedRange[row.Row, i]).Value.ToString(), out currReq);
+                                if (isNumber)
+                                {
+                                    if (currReq > stock)
+                                    {
+                                        break;
+                                    }
+                                    
+                                    //decrease remaining stock
+                                    stock -= currReq;
+                                }
+                            }
+                            //paint
+                            ((Range)UsedRange[row.Row, i]).Interior.Color = Color.LightGray;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnHideCoverage_Click(object sender, RibbonControlEventArgs e)
+        {
+                        Workbook wb = Globals.ThisAddIn.Application.ActiveWorkbook;
+            Worksheet sht = wb.ActiveSheet;
+
+            Range UsedRange = sht.UsedRange;
+            foreach(Range row in UsedRange.Rows)
+            {
+                for (int i = FirstColumn; i < UsedRange.Columns.Count - 1; i++)
+                {
+                    if (((Range)UsedRange[row.Row, i]).Interior.Color == ColorTranslator.ToOle(System.Drawing.Color.LightGray))
+                    {
+                        ((Range)UsedRange[row.Row, i]).Interior.Color = Color.Transparent;
+                    }
+                }
+            }
+        }
+    }
+}
